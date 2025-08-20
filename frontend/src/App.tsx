2b58ee1b3364c729841099ffe96874c38ad4cdea
Manpreet Singh <manpreets95828@gmail.com>
2025-08-20 18:33:05 +0530

    Frontend of chat application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,62 @@
-import { useEffect, useState } from 'react';
-import { useRef } from 'react';
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [socket, setSocket] = useState();
-  const inputRef = useRef();
-
-  function sendMessage(){
-    if(!socket){
-      return;  
-    }
-    const message = inputRef.current.value;
-    //@ts-ignore
-    socket.send(message);
-  }
+  const [messages, setMessages] = useState(["hi there", "this is here"]);
+  const wsRef = useRef();
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080")
-    setSocket(ws);
-
-    ws.onmessage = (ev) => {
-      alert("Message received:" + ev.data);
-    }
-
-    
-  }, [])
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current = ws;
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+  }, []);
 
   return (
-    <div>
-      <input ref={inputRef} type='text' placeholder='Message...'/>
-      <button onClick={sendMessage}>Send</button>
+    <div className="h-screen bg-black ">
+      <br /> <br /> <br /> <br />
+      <div className="h-[95vh]">
+        {messages.map((message) => (
+          <div className="m-8">
+            <span className="bg-white text-black rounded p-4 ">
+              {message}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="w-full bg-white flex p-4">
+        <input type="text" id="message" className="flex1 border border-black rounded-sm" />
+        <button
+          onClick={() => {
+            const message = document.getElementById("message")?.value;
+            wsRef.current.send(
+              JSON.stringify({
+                type: "chat",
+                payload: {
+                  message: message,
+                },
+              })
+            );
+          }}
+          className="bg-purple-600 text-white rounded-sm"
+        >
+          Send Message
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
